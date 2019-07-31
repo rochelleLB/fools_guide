d90d29602c0f0648aa88032eb997946d2402c10a
Rochelle Burrows <rochelleburrows@Rochelles-MacBook-Pro.local>
2019-07-31 01:29:10 +0100

    linked pages and connected external API

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "pug");
+
+//EKelen tarot API
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/card-of-the-day", (req, res) => {
-  res.send("Your card of the day");
+  res.render("card-of-the-day");
 });
 
 app.get("/tarot-reading", (req, res) => {
-  res.send("Choose a spread for your own personalised reading");
+  res.render("tarot-reading");
 });
 
 app.get("/browse-cards", (req, res) => {
-  res.send("Tarot Card Database");
+  const tarotDB = {
+    url: `https://rws-cards-api.herokuapp.com/api/v1/cards`
+  };
+  request(tarotDB, (error, response, body) => {
+    // temp bodyParser
+    let cards = JSON.parse(body);
+    // to rename later
+    let tarotCards = cards.cards;
+    console.log(tarotCards[0].name);
+    res.render("browse-cards", { tarotCards: tarotCards });
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About");
+  res.render("about");
 });
 
 app.listen(3000, () => {
-  console.log("The server is running!")
+  console.log("The server is running!");
 });
